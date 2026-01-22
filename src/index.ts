5e31dc3a08f1a43f8b5773fab0dfcfec99de06a0
Theo Sementa <theosementa@gmail.com>
2026-01-22 11:43:13 +0100

    x

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 export * from './auth/dtos/set-refresh-token.dto';
 export * from './auth/dtos/tokens.dto';
 export * from './auth/events/auth-event.enum';
+export * from './logs/body/log.body';
 export * from './logs/events/log-event.enum';
 export * from './user/body/user.body';
 export * from './user/dtos/user.dto';
 export * from './user/enums/user-role.enum';
 export * from './utils/date.utils';
 export * from './utils/rpc-validation.pipe';
+
